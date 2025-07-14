92024ff243af8945ef14523bcbf92231c613b064
Devanshu shekhar <Devanshushekhar1@outlook.com>
2025-07-14 19:10:54 +0530

    added module folder

--- Infra/main.tf
+++ Infra/main.tf
@@ -12,26 +12,31 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "billing_rg" {
-  name     = "billing-rg"
+  name     = var.resource_group
   location = var.location
 }
 
+# Load Cosmos DB setup
 module "cosmos" {
   source = "./cosmos.tf"
 }
 
+# Load Blob Storage setup
 module "blob" {
   source = "./blob.tf"
 }
 
+# Load Function App and Durable Functions
 module "functions" {
   source = "./functions.tf"
 }
 
+# Load Azure CDN configuration
 module "cdn" {
   source = "./cdn.tf"
 }
 
+# Load monitoring and alerting setup
 module "monitoring" {
   source = "./monitoring.tf"
 }
